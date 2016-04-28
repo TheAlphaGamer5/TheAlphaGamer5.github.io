194269a0ad0206db4959df37d6bfc97c91828639
aheusser <ngonzalezheusser@gmail.com>
2016-04-28 11:05:11 -0700

    things work again yay!

--- public_html/js/Admin.js
+++ public_html/js/Admin.js
@@ -1,17 +1,9 @@
 $(function () {
-<<<<<<< HEAD
    var APPLICATION_ID = "20A6F9A5-CC8A-794B-FF49-615D1FBBCF00",
        SECRET_KEY = "41F1FEBE-827C-7DD6-FFED-8906A8EC1100",
        VERSION = "v1";
        
        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
-=======
-   var APPLICATION_ID = "5AA1389C-DF5C-C2AD-FF36-446AC68B6C00",
-       SECRET_KEY = "44AB01C4-A20F-AE1B-FFDC-FEAAA9841100",
-      VERSION = "v1";
-      
-  Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
->>>>>>> f91185cf75306468f22f7bef103faad6248141c2
   if(Backendless.UserService.isValidLogin()){
       userLoggedIn(Backendless.LocalCache.get("current-user-id"));
   }else{
@@ -29,7 +21,6 @@ $(function () {
       Backendless.UserService.login(email, password, true, new Backendless.Async(userLoggedIn, gotError));
   });
   
-<<<<<<< HEAD
   $(document).on('click', '.add-task', function(){
       var addTaskScript = $("#add-task-template").html();
   var addTaskTemplate = Handlebars.compile(addTaskScript);
@@ -38,24 +29,14 @@ $(function () {
   tinymce.init({ selector:'textarea'});
   });
   $(document).on('submit', '.form-add-task', function (event){
-=======
-  $(document).on('click', '.add-blog', function(){
-      var addBlogScript = $("#add-blog-template").html();
-  var addBlogTemplate = Handlebars.compile(addBlogScript);
-  
-  $('.main-container').html(addBlogTemplate);
-  tinymce.init({ selector:'textarea'});
-  });
-  $(document).on('submit', '.form-add-blog', function (event){
->>>>>>> f91185cf75306468f22f7bef103faad6248141c2
       event.preventDefault();
       
       var data = $(this).serializeArray(),
           title = data[0].value,
           content = data[1].value;
           
-        if(content === "" || title === "") {
-             Materialize.toast('Add Title and Content', 1500);
+        if(title === "") {
+             Materialize.toast('Add Task and/or Notes', 1500);
          }
          else {
          var dataStore = Backendless.Persistence.of(Posts);
